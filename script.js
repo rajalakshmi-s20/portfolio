488c6155fcb4da403255ab7954616c8dbd740560
RAJALAKSHMI.S <rajalakshmi.sarangan20@gmail.com>
2023-04-25 23:42:52 +0530

    updated

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
